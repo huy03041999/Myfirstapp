bc6a445109c45a5b88a96200b9ebf7d4b97a5416
HuyBanh <huynqgch17107@fpt.edu.vn>
2019-04-24 13:57:42 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Store</title>
+</head>
+<body>
+	<h1>My first PHP page</h1>
+
+	<?php echo "hello" ;?>
+	
+</body>
+</html>
