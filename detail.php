3acd934cae54778fedbc715c8ed74c78e8fbc72f
HuyBanh <huynqgch17107@fpt.edu.vn>
2019-05-05 18:24:24 +0700

    aaa

--- detail.php
+++ detail.php
@@ -5,7 +5,7 @@
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap-theme.min.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
-	<title>GearCl</title>
+	<title>ATNshop</title>
 	<style>
 		
 		.thumbnail img{
