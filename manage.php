a057639a8c22d0a1be9bbe89167e50b63897376c
HuyBanh <huynqgch17107@fpt.edu.vn>
2019-05-06 13:43:23 +0700

    aaa

--- manage.php
+++ manage.php
@@ -1,11 +1,11 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
-    <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap-theme.min.css">
-    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
-    <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
-	<title>GearCl</title>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap-theme.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
+	<title>ATN</title>
 	<style>
 		
 		.thumbnail img{
@@ -24,24 +24,21 @@
 <body>
 	<?php 
 		if (isset($_POST['oldproid'])) {
-			require_once ('./dbconnector.php');
-			$con=new DBConnector();
+			include 'dbconnector.php';
 			$sql="UPDATE `product` SET `proname`='".$_POST['proname']."',`proimage`='".$_POST['proimage']."',`price`='".(int)$_POST['price']."',`prodes`='".$_POST['prodes']."',`cateid`='".(int)$_POST['cateid']."' WHERE proid=".$_POST['oldproid'];
 			$con -> execStatement($sql);
 		}
 	 ?>
 	<?php 
 		if (isset($_GET['del'])) {
-			require_once ('./dbconnector.php');
-			$con=new DBConnector();
+			include 'dbconnector.php';
 			$sql = "DELETE FROM `product` WHERE proid=".$_GET['del'];
 			$con -> execStatement($sql);
 		}
 	 ?>
 	<?php 
 		if (isset($_POST['proid'])) {
-			require_once ('./dbconnector.php');
-			$con=new DBConnector();
+			include 'dbconnector.php';
 			$sql="INSERT INTO `product`(`proid`, `proname`, `proimage`, `price`, `prodes`, `cateid`) VALUES ('".(int)$_POST['proid']."','".$_POST['proname']."','".$_POST['proimage']."','".(int)$_POST['price']."','".$_POST['prodes']."','".(int)$_POST['cateid']."')";
 			$con -> execStatement($sql);
 		}
@@ -59,16 +56,15 @@
 		
 				<div class="collapse navbar-collapse navbar-ex1-collapse">
 					<ul class="nav navbar-nav">
-						<li><a href="ass2webdesign.php">Home</a></li>
+						<li><a href="index.php">Home</a></li>
 						<li><a href="#">Manage</a></li>
 <?php 
-	require_once ('./dbconnector.php');
-	$con=new DBConnector();
-	$sql=("select * from category");
-	$rows = $con->runQuery($sql);
-	foreach($rows as $r)
+	include 'dbconnector.php';
+	$sqlcate="select * from category";
+	$resultcate = pg_query($connection, $sqlcate);
+	while($rows = pg_fetch_assoc($resultcate))
 	{?>
-						<li><a href="detail.php?category=<?=$r['cateid']?>"><?=$r['catename']?></a></li>	
+						<li><a href="category.php?category=<?php echo $rows['cateid']?>"><?php echo $rows['catename']?></a></li>	
 	<?php
 	}
  ?>
@@ -85,27 +81,23 @@
 
 	<div class="container">
 		<?php  
-	require_once('./dbconnector.php');
+	include'dbconnector.php';
 	if(isset($_GET['productname']))
 	{
 		$productname = $_GET['productname'];
-		$con = new DBConnector();
-		$sql = "Select * from product where proname like '%".$productname."%'";
-		$rows = $con -> runQuery($sql);
-		foreach($rows as $r)
+		$sqlsearch = "Select * from product where proname like '%".$productname."%'";
+		$resultsearch = pg_query($connection, $sqlsearch);
+		while($rows = pg_fetch_assoc($resultsearch))
 	{?>		
 			<div class="item  col-xs-3 col-lg-3"> 
-				<div class="thumbnail"> <img class="group list-group-image" src="<?=$r['proimage']?>" alt="<?=$r['proname']?>" width="300"> 
+				<div class="thumbnail"> <img class="group list-group-image" src="<?php echo $rows['proimage']?>" alt="<?php echo $rows['proname']?>" width="300"> 
     				<div class="caption"> 
-    						<h4 class="group inner list-group-item-heading"> <?=$r['proname']?></h4> 
-     						<p class="group inner list-group-item-text"><?=$r['prodes']?></p> 
+    						<h4 class="group inner list-group-item-heading"> <?php echo $rows['proname']?></h4> 
+     						<p class="group inner list-group-item-text"><?php echo $rows['prodes']?></p> 
      					<div class="row"> 
       						<div class="col-xs-12 col-md-6"> 
-      							 <p class="lead"><?=$r['price']?></p> 
+      							 <p class="lead"><?php echo $rows['price']?></p> 
      						</div> 
-    		 			<div class="col-xs-12 col-md-6"> 
-    		 				<a class="btn btn-success">Go</a> 
-      					</div> 
      					</div> 
    					</div> 
   				</div> 
@@ -116,24 +108,22 @@
 ?>	
 				
 <?php 
-	require_once ('./dbconnector.php');
-	$con=new DBConnector();
-	$sql=("select * from product");
-	$rows = $con->runQuery($sql);
-	foreach($rows as $r)
+	include 'dbconnector.php';
+	$sql="select * from product";
+	$result = pg_query($connection,$sql);
+	while($rows = pg_fetch_assoc($result))
 	{?>		
 			<div class="item  col-xs-3 col-lg-3"> 
-				<div class="thumbnail"> <img class="group list-group-image img-responsive" src="<?=$r['proimage']?>" alt="<?=$r['proname']?>" width="300"> 
+				<div class="thumbnail"> <img class="group list-group-image" src="<?php echo $rows['proimage']?>" alt="<?php echo $rows['proname']?>" width="300"> 
     				<div class="caption"> 
-    						<h4 class="group inner list-group-item-heading"> <?=$r['proname']?></h4> 
+    						<h4 class="group inner list-group-item-heading"> <?php echo $rows['proname']?></h4> 
      						<p class="group inner list-group-item-text"><?=$r['prodes']?></p> 
      					<div class="row"> 
       						<div class="col-xs-12 col-md-6"> 
-      							 <p class="lead"><?=$r['price']?>$</p> 
+      							 <p class="lead"><?php echo $rows['price']?>$</p> 
      						</div> 
     		 			<div class="col-xs-12 col-md-6"> 
-    		 				<a class="btn btn-danger" onclick="return deleteqry(<?php echo $r['proid'] ?>)">Delete</a> 
-    		 				<a href="edit.php?id=<?php echo $r['proid'] ?>" class="btn btn-primary">Edit</a>
+    		 				<a class="btn btn-success"  href="detail.php?proid=<?php echo $rows['proid']?>">Go</a> 
       					</div> 
      					</div> 
    					</div> 
